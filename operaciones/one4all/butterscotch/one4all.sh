a18be78c2097d8c3fb5d8a55d4ace71bcf641c84
Raul Rosas <raulolivaresrosas@gmail.com>
2023-04-26 02:06:05 -0600

    Corrección de errores de llamado de archivos

--- operaciones/one4all/butterscotch/one4all.sh
+++ operaciones/one4all/butterscotch/one4all.sh
@@ -18,9 +18,6 @@ echo "
 Este script esta diseñado para trabajar con tablas de hits (--domtblout) por dominio de HMMER!
 ";
 
-mkdir resultados;
-mkdir /resultados/control_lists;
-
 # Quita encabezados y colas de archivo que empiecen con el caracter "#"
 egrep -v "^#" $1 > noheadtail.temp;
 
@@ -31,7 +28,7 @@ rm noheadtail.temp;
 # Lista control de campos
 mv tabbed.tab raw_fields.temp;
 cut -f1-22 raw_fields.temp > data.temp;
-cut -f23- |  sed 's/\t/ /g' | sed 's/,/ /g' > description.temp;
+cut -f23- raw_fields.temp |  sed 's/\t/ /g' | sed 's/,/ /g' > description.temp;
 paste data.temp description.temp > all_fields.tab;
 
 # Sacamos campos de interés, 11/23 en específico.
@@ -40,15 +37,17 @@ cut -f1,3,4,7,16-21,23 all_fields.tab > dom_fields.tab;
 # Borramos intermedios
 rm data.temp;
 rm description.temp;
-rm dom_fields.temp;
+
+mkdir resultados;
+mkdir resultados/control_lists;
 
 # -|YAMATO|- Filtrado a nivel E-value y GeneID
 
 # E-values: Solo los resultados con un e-value mayor o igual a 0.0001 se conservan
-awk '$7 <= 0.0001' dom_fields.tab > good_evalues.tab
+awk '$4 <= 0.0001' dom_fields.tab > good_evalues.tab
 
 # De los dominios, toma solo al primero. Esto solo se hace tomando el primer resultado.
-awk '!a[$1]++' good_values.tab > firstdom_only.tab
+awk '!a[$1]++' good_evalues.tab > firstdom_only.tab
 
 #GeneID: Toma como llave la clave de acceso de dom_fields para buscar su homonimo en la base GTF, la imprime y la filtra por duplicados utilizando el GenID
 echo "El archivo all_fields.tab será tu Checkpoint 1, guardalo bien!";
